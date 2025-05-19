c62160a2a45ae3d2adf6b7744cb09e16d99a3423
Drake Redfield <drake.redfield@hotmail.com>
2025-05-18 19:00:32 -0600

    feat: longest palindrome test added

--- sliding_window/longest_palindromic_substring/index.test.ts
+++ sliding_window/longest_palindromic_substring/index.test.ts
@@ -1,5 +1,28 @@
-describe('', () => {
-  it('', () => {
-    
-  })
+import { longestPalindromicSubstring } from "./longest_palindromic_substring"
+
+describe('Longest Palindrom', () => {
+  it('Get all longest Palindrom from "babad"', () => {
+    const result = longestPalindromicSubstring('babad');
+    expect(result).toEqual(['bab', 'aba']);
+  });
+
+  it('Get all longest Palindrom from "cbbd"', () => {
+    const result = longestPalindromicSubstring('cbbd');
+    expect(result).toEqual(['bb']);
+  });
+
+  it('Get all longest Palindrom from "applppaldsbb"', () => {
+    const result = longestPalindromicSubstring('applppaldsbb');
+    expect(result).toEqual(['applppa']);
+  });
+
+  it('Must return every letter from "abcdefg"', () => {
+    const result = longestPalindromicSubstring('abcdefg');
+    expect(result).toEqual('abcdefg'.split(''));
+  });
+
+  it('Must return an empty list with empty string ""', () => {
+    const result = longestPalindromicSubstring('');
+    expect(result).toEqual([]);
+  });
 })
